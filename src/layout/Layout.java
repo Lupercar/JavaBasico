ba287d4e1c4e114a5912a2ef026f1d644098fcf6
Lupercar <hotolete@gmail.com>
2017-06-11 22:08:56 +0200

    Dos laminas, la 2 machaca a la 1

--- src/layout/Layout.java
+++ src/layout/Layout.java
@@ -29,6 +29,9 @@ class MarcoLayout extends JFrame{
 		PanelLayout lamina = new PanelLayout(); 
 		
 		add(lamina); //agregamos la lámina
+		
+		PanelLayout2 lamina2 = new PanelLayout2(); 
+		add(lamina2);
 	}
 }
 
@@ -36,10 +39,19 @@ class PanelLayout extends JPanel{
 	public PanelLayout() {
 		
 		//establecemos el posicionamiento de la lámina en el constructor
-		setLayout(new BorderLayout(10,10));
+		setLayout(new FlowLayout(FlowLayout.LEFT));
 		
-		add(new JButton("Amarillo"), BorderLayout.NORTH);
-		add(new JButton("Azul"), BorderLayout.WEST);
+		add(new JButton("Amarillo"));
+		add(new JButton("Azul"));
+	}
+}
+
+//ponemos una segunda lámina
+class PanelLayout2 extends JPanel{
+	public PanelLayout2(){
+		
+		setLayout(new BorderLayout());
+		
 		add(new JButton("Rojo"), BorderLayout.SOUTH);
 		add(new JButton("Verde"), BorderLayout.EAST);
 		add(new JButton("Negro"), BorderLayout.CENTER); 
