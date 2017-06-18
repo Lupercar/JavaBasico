20ed4aa6840c493eccac0b31fbb600c66373e058
Lupercar <hotolete@gmail.com>
2017-06-18 14:15:15 +0200

    Creamos un JTextField con constructor por defecto

---
+++ src/componentesSwing/PruebaTexto.java
@@ -0,0 +1,30 @@
+package componentesSwing;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class PruebaTexto {
+
+	public static void main(String[] args) {
+		MarcoTexto miMarco = new MarcoTexto();
+		miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
+
+class MarcoTexto extends JFrame{
+	public MarcoTexto(){
+		setTitle("Prueba Texto");
+		setBounds(600, 300, 600, 350);
+		setVisible(true);
+		LaminaTexto miLamina = new LaminaTexto(); 
+		add(miLamina); 
+	}
+}
+
+class LaminaTexto extends JPanel{
+	public LaminaTexto(){
+		JTextField campo1 = new JTextField(); 
+		add(campo1); 
+	}
+}
