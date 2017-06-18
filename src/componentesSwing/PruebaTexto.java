08cc5a253ef8c8efc3e0bf087ee340ff2011517e
Lupercar <hotolete@gmail.com>
2017-06-18 14:29:56 +0200

    Utilizo constructor JTextField del ancho de columnas, de tipo int

--- src/componentesSwing/PruebaTexto.java
+++ src/componentesSwing/PruebaTexto.java
@@ -24,7 +24,7 @@ class MarcoTexto extends JFrame{
 
 class LaminaTexto extends JPanel{
 	public LaminaTexto(){
-		JTextField campo1 = new JTextField("Texto por defecto"); 
+		JTextField campo1 = new JTextField(20); 
 		add(campo1); 
 	}
 }
