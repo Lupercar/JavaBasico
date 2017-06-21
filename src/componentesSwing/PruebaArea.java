27a26a8e43eca6c0b1ca4f1616b40d81629edbb8
Lupercar <hotolete@gmail.com>
2017-06-21 10:54:20 +0200

    Le doy funcionalidad al botón y creo un JTextArea

--- src/componentesSwing/PruebaArea.java
+++ src/componentesSwing/PruebaArea.java
@@ -1,10 +1,13 @@
 package componentesSwing;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 //crearé un gran área de texto en la zona central y abajo 2 botones.
 public class PruebaArea {
@@ -30,6 +33,14 @@ class MarcoPruebaArea extends JFrame{
 		
 		//inicio botones
 		botonInsertar = new JButton("Insertar");
+		
+		botonInsertar.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				areaTexto.append("En un lugar de la mancha de cuyo nombre no quiero acordarme"); 
+			}
+		});
 	}
 	
 	//defino un objeto de tipo lámina
@@ -37,4 +48,7 @@ class MarcoPruebaArea extends JFrame{
 	
 	//defino botones
 	private JButton botonInsertar, botonSaltoLinea; 
+	
+	//defino area de texto
+	private JTextArea areaTexto; 
 }
